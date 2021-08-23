ef95110b99935c0a409f30fab30b39a66e6dde75
brandon206 <brandonpark206@gmail.com>
2021-08-22 23:07:10 -0700

    Frontend to Backend Comunication
    - Installing axios and node-sass on client side
    - Updating App.js to use App.scss file and adding basic input and post functionality to DB
    - Bugfix change for calling express.json method. using cors package to prevent cors errors
    - Creating /register post route to add user to the database
    - Minor css adds in App.scss file

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,41 @@
+import "./App.scss";
+import React, { useState } from "react";
+import Axios from "axios";
+
 function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const register = async () => {
+    try {
+      const response = await Axios.post("http://localhost:5000/register", {
+        name: username,
+        status: "active",
+        role: "user",
+      });
+      console.log(response.data);
+      alert('is working!');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="App">
-      Application Here
+      <div className="inputs">
+        <input
+          type="text"
+          placeholder="username"
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          onChange={(event) => setPassword(event.target.value)}
+        />
+
+        <button onClick={register}>Register</button>
+      </div>
     </div>
   );
 }
