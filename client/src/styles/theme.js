a4a580ccddef4b128d0f70e1939f8cb0a22c948d
brandon206 <brandonpark206@gmail.com>
2021-08-29 21:10:15 -0700

    Frontend Page Scaffolding and Navigation
    - adding chakraUI component framework
    - adding basic routing through react-router-dom
    - updating App.js with router
    - Creating Components
        - SignUp.js
        - Home.js
        - Login.js
    - adding local sass files for styling
    - updating default theme for chakra

---
+++ client/src/styles/theme.js
@@ -0,0 +1,14 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const metTheme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: "#061423",
+        color: "white",
+      },
+    },
+  },
+});
+
+export default metTheme;
