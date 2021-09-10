372cf20c5ec44fe2832578938ab1f2c0c3c62838
brandon206 <brandonpark206@gmail.com>
2021-09-09 21:55:03 -0700

    Google OAuth Improvements - adding react-google-login package to handle google oauth - removing cruft for GoogleLoginButton component - updating GoogleIcon svg - updating NavbarLinks component to correctly handle onClick method and showing/hiding login/logout - adding new functionality to Navbar-new component that handles storing the google profile and handling login/logout - wip for login component, need to add jwt token functionality

--- client/src/pages/SignUp/SignUp.js
+++ client/src/pages/SignUp/SignUp.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import GoogleLoginButton from "../../components/GoogleLoginButton/GoogleLoginButton";
 
 const SignUp = () => {
   const [email, setEmail] = React.useState("");
